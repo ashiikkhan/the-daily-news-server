9d0d78414297bfc7c7d838f51b397c55b7c2e84d
ashiikkhan <ashiksust06@gmail.com>
2022-10-27 12:40:25 +0600

    all done

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(cors());
 
 //category data
 const categories = require('./data/categories.json');
-//images:
-const brands = require('./data/brands.json');
+//news data
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
   res.send('news api runnig ');
@@ -19,8 +19,25 @@ app.get('/news-categories', (req, res) => {
   res.send(categories);
 });
 
-app.get('/brands', (req, res) => {
-  res.send(brands);
+app.get('/news', (req, res) => {
+  res.send(news);
+});
+
+app.get('/category/:id', (req, res) => {
+  const id = req.params.id;
+  if (id === '08') {
+    res.send(news);
+  } else {
+    const category_news = news.filter((n) => n.category_id === id);
+    res.send(category_news);
+  }
+});
+
+//news req & respond
+app.get('/news/:id', (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 
 app.listen(port, () => {
